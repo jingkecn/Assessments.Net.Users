5b826501007c200341d4eaa5d9154aa512787d77
Jing KE <jingkecn@gmail.com>
2025-03-25 00:34:00 +0100

    [project] cleanup projects.

--- src/Starter.AppHost/Program.cs
+++ src/Starter.AppHost/Program.cs
@@ -2,13 +2,20 @@ using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var db = builder.AddSqlServer("database")
-    .WithDataVolume();
+var password = builder
+    .AddParameter("assessments-mssql-password", "$tr0ngPa$$w0rd", secret: true);
+var mssql = builder
+    .AddSqlServer("assessments-mssql", password, 44000);
 
-builder.AddProject<Command_Api>("command-api")
-    .WithReference(db);
+var defaultDb = mssql
+    .AddDatabase("users-db");
 
-builder.AddProject<Query_Api>("query-api")
-    .WithReference(db);
+builder.AddProject<Command_Api>("users-command-api")
+    .WithReference(defaultDb)
+    .WaitFor(defaultDb);
+
+builder.AddProject<Query_Api>("users-query-api")
+    .WithReference(defaultDb)
+    .WaitFor(defaultDb);
 
 builder.Build().Run();
